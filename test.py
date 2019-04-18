56c220ef0ec37f48688f0c882fff930caac27765
義川　智也 <swa03326@gikawatomoyanoMacBook-Pro.local>
2019-04-18 11:24:22 +0900

    ajdksbvnkad

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import torch
+import torch.utils.data.DataLoader as DataLoader
 from cnn_finetune import make_model
 from sklearn.metrics import classification_report
 import os
 from MyDataset import MyDataset
-
+from inception import inception_v3
 
 def test():
     model.eval()
@@ -17,15 +18,23 @@ def test():
 
     print(classification_report(Y, pred))
 
+config = {
+    'dataset_dir': 'dataset/flower_images/',
+    'label_file': 'dataset/flower_images/flower_labels.csv',
+    'num_classes': 10,
+    'batch_size': 70,
+}
 
 dataset_dir = 'dataset/flower_images/'
 label_file= 'dataset/flower_images/flower_labels.csv'
 
 test_set = MyDataset(label_file, dataset_dir)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=210, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=config['batch_size'], shuffle=True)
 
-model = make_model('inception_v3', num_classes=10, input_size=(224, 224))
-param = torch.load('model/cnn_dict.model')
-model.load_state_dict(param)
+#model = make_model('inception_v3', num_classes=config['num_classes'], input_size=(224, 224))
+model = inception_v3(num_classes=config['num_classes'],
+                     transform_input=True)
+
+model.load_state_dict(torch.load('model/cnn_dict.model'))
 
 test()
