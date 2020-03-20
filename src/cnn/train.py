3fe3f2030e5d677eb5113c0bea2a706be9aa40eb
yoshikawat64m <yoshikawat.64m@gmail.com>
2020-03-20 22:23:10 +0900

    Update train.py

--- src/cnn/train.py
+++ src/cnn/train.py
@@ -4,66 +4,83 @@ import torch.nn as nn
 import torch.optim as optim
 
 import datetime
-import matplotlib.pyplot as plt
-import os
 
-from MyDataset import MyDataset
+from dataset import MyDataset
 
-from inception import inception_v3
+from models.inception.inception_v3 import inception_v3
 
 
-def train(epoch):
-    total_loss = 0
-    total_size = 0
-    # import pdb; pdb.set_trace()
+class TrainModel:
 
-    model.train()
+    def __init__(self):
+        config = {
+            'dataset_dir': 'dataset/flower_images/',
+            'label_file': 'dataset/flower_images/flower_labels.csv',
+            'num_classes': 10,
+            'batch_size': 70,
+            'num_epochs': 30,
+            'pretrained': False,
+            'input_size': 299,
+        }
 
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+        dataset = MyDataset(
+            config['label_file'],
+            config['dataset_dir'],
+            size=config['input_size']
+        )
+        self.data_loader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=config['batch_size'],
+            shuffle=True
+        )
 
-        output = model(data)
-        loss = criterion(output[0], target)
+        if torch.cuda.is_available():
+            self.device = torch.device('cuda')
+        else:
+            self.device = torch.device('cpu')
 
-        total_loss += loss.item()
-        total_size += data.size(0)
+        self.model = inception_v3(
+            pretrained=config['pretrained'],
+            num_classes=config['num_classes'],
+            transform_input=True
+        ).to(self.device)
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    def run_train(self, num_epochs):
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(self.model.parameters())
 
-        if batch_idx % 1000 == 0:
-            now = datetime.datetime.now()
-            print('[{}] Train Epoch: {} [{}/{} ({:.0f}%)]\tAverage loss: {:.6f}'.format(
-                now,
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100 * batch_idx / len(train_loader), total_loss / total_size))
+        for epoch in range(1, num_epochs + 1):
+            self._train(epoch)
 
+        torch.save(self.model.state_dict(), 'model/cnn_dict.model')
+        torch.save(self.model, 'model/cnn.model')
 
-config = {
-    'dataset_dir': 'dataset/flower_images/',
-    'label_file': 'dataset/flower_images/flower_labels.csv',
-    'num_classes': 10,
-    'batch_size': 70,
-    'num_epochs': 30,
-    'pretrained': False,
-    'input_size': 299,
-}
+    def _train(self, epoch):
+        total_loss = 0
+        total_size = 0
 
-train_set = MyDataset(config['label_file'], config['dataset_dir'], size=config['input_size'])
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=config['batch_size'], shuffle=True)
+        self.model.train()
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        for batch_idx, (data, target) in enumerate(self.data_loader):
+            data, target = data.to(self.device), target.to(self.device)
 
-model = inception_v3(pretrained=config['pretrained'],
-                     num_classes=config['num_classes'],
-                     transform_input=True).to(device)
+            output = self.model(data)
+            loss = self.criterion(output[0], target)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters())
+            total_loss += loss.item()
+            total_size += data.size(0)
 
-for epoch in range(1, config['num_epochs'] + 1):
-    train(epoch)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
 
-torch.save(model.state_dict(), 'model/cnn_dict.model')
-torch.save(model, 'model/cnn.model')
+            if batch_idx % 1000 == 0:
+                now = datetime.datetime.now()
+                print('[{}] Train Epoch: {} [{}/{} ({:.0f}%)]\tAverage loss: {:.6f}'.format(
+                    now,
+                    epoch,
+                    batch_idx * len(self.data),
+                    len(self.data_loader.dataset),
+                    100 * batch_idx / len(self.data_loader),
+                    total_loss / total_size
+                ))
